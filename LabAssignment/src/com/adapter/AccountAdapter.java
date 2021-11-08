3f123f8f2f8c9e4c1b1589e84fe1066ad6211ae4
Ahzam Shahnil <62467164+ahzam-shahnil@users.noreply.github.com>
2021-11-08 09:06:39 +0500

    lab task by  ahzam

---
+++ LabAssignment/src/com/adapter/AccountAdapter.java
@@ -0,0 +1,23 @@
+package com.adapter;
+
+
+public class AccountAdapter extends AbstractAccount {
+
+
+    private OffshoreAccount offshoreAccount;
+
+    public AccountAdapter( OffshoreAccount offshoreAccount) {
+        super(offshoreAccount.getOffshoreBalance());
+        this.offshoreAccount = offshoreAccount;
+    }
+
+    @Override
+    public double getBalance() {
+        final double taxRate = offshoreAccount.getTaxRate();
+        final double grossBalance = offshoreAccount.getOffshoreBalance();
+
+        final double taxableBalance = grossBalance * taxRate;
+        final double balanceAfterTax = grossBalance - taxableBalance;
+        return balanceAfterTax;
+    }
+}
